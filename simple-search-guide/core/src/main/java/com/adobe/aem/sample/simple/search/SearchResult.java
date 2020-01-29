5ac9a9612f986e84c5b6ae107f718b724da147f2
Raj Mishra <rajm@deloitte.com>
2020-01-29 09:12:28 +0530

    first commit

---
+++ simple-search-guide/core/src/main/java/com/adobe/aem/sample/simple/search/SearchResult.java
@@ -0,0 +1,30 @@
+package com.adobe.aem.sample.simple.search;
+
+import aQute.bnd.annotation.ProviderType;
+
+import java.util.Collection;
+import java.util.List;
+
+@ProviderType
+public interface SearchResult {
+    int DESCRIPTION_MAX_LENGTH = 320;
+
+    String DESCRIPTION_ELLIPSIS = " ... ";
+
+    enum ContentType {
+        PAGE,
+        ASSET
+    }
+
+    ContentType getContentType();
+
+    String getURL();
+
+    String getPath();
+
+    String getTitle();
+
+    String getDescription();
+
+ 
+}
